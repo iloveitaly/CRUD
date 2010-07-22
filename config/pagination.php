b1172feacbdea14ef1d5fd1c0f596842ced7dffb
Michael Bianco <iloveitaly@gmail.com>
2010-07-21 23:11:04 -0400

    Adding default pagination values

---
+++ config/pagination.php
@@ -0,0 +1,11 @@
+<?
+$config['default'] = array
+(
+	'directory'      => 'pagination',
+	'style'          => 'classic',
+	'uri_segment'    => 3,
+	'query_string'   => 'p',
+	'items_per_page' => 150,
+	'auto_hide'      => TRUE,
+);
+?>
