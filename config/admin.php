5c3e65236bfa019c8c49fb03897a08e85285d696
Michael Bianco <iloveitaly@gmail.com>
2010-12-14 09:42:50 -0500

    Added CKEditor JS config to the admin.php

--- config/admin.php
+++ config/admin.php
@@ -8,3 +8,21 @@ $config['timezone_offset'] = 0;
 
 $config['base'] = '/';
 $config['manage_relationships'] = true;
+
+$config['editor_javascript'] = <<<EOL
+var target = $$(".editor");
+
+if(target.length > 0) {
+	target = target[0];
+
+	// remove the label
+	$$("label[for=\'" + target.getProperty("id") + "\']")[0].destroy();
+
+	// CKEDITOR.plugins.add("CKEDITOR.plugins.add");
+	CKEDITOR.replace(target, {
+		height:"500px"
+	});
+}
+
+EOL;
+?>
