99bbdb9cb4e22cec615bbabe4ef912c283284193
Michael Bianco <mbianco@ascensionpress.com>
2011-07-25 15:00:38 -0400

    Added dropdown ability for module list

--- views/header.php
+++ views/header.php
@@ -1,13 +1,27 @@
-<div id="header">
+<header>
 <?if(Kohana::config('admin.header')):?>
 	<div id="header_info" class="wrapper">
 		<a href="<?=Kohana::config("admin.base")?>login/logout" style="float:right; text-align:right;">Logout &rarr;</a>
 		<a href="<?=url::base()?>" target="_blank">&larr; Visit Site</a>
 	</div>
 <?endif;?>
-	<ul class="tabs">
-<?foreach(Kohana::config('admin.sections') as $name => $path):?>
-		<li><a href="<?=Kohana::config('admin.base').$path?>/"<?=strpos(url::current(), $path) !== FALSE ? ' class="selected"' : ''?>><?=$name?></a></li>
+	<nav>
+		<ul>
+<?
+foreach(Kohana::config('admin.sections') as $name => $path):
+	if(is_array($path)):
+?>
+	<li>
+		<a href=""><?=$name?></a>
+		<ul>
+<?foreach($path as $subName => $subPath):?>
+			<li><a href="<?=Kohana::config('admin.base').$subPath?>"><?=$subName?></a></li>
 <?endforeach;?>
-	</ul>
-</div>
+		</ul>
+	</li>
+<?else:?>
+	<li><a href="<?=Kohana::config('admin.base').$path?>/"<?=strpos(url::current(), $path) !== FALSE ? ' class="selected"' : ''?>><?=$name?></a></li>
+<?endif;endforeach;?>
+		</ul>
+	</nav>
+</header>
