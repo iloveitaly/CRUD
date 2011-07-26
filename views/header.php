f2ae61ed7a7bd72d631be118465beb8abedee268
Michael Bianco <mbianco@ascensionpress.com>
2011-07-26 13:29:21 -0400

    Minor header link tweaks

--- views/header.php
+++ views/header.php
@@ -1,7 +1,7 @@
 <header>
 <?if(Kohana::config('admin.header')):?>
 	<div id="header_info" class="wrapper">
-		<a href="<?=Kohana::config("admin.base")?>login/logout" style="float:right; text-align:right;">Logout &rarr;</a>
+		<a href="<?=join_paths(Kohana::config("admin.base"), 'login/logout')?>" style="float:right; text-align:right;">Logout &rarr;</a>
 		<a href="<?=url::base()?>" target="_blank">&larr; Visit Site</a>
 	</div>
 <?endif;?>
@@ -12,15 +12,15 @@ foreach(Kohana::config('admin.sections') as $name => $path):
 	if(is_array($path)):
 ?>
 	<li>
-		<a href=""><?=$name?></a>
+		<a href="#"><?=$name?></a>
 		<ul>
 <?foreach($path as $subName => $subPath):?>
-			<li><a href="<?=Kohana::config('admin.base').$subPath?>"><?=$subName?></a></li>
+			<li><a href="join_paths(<?=Kohana::config('admin.base'), $subPath)?>"><?=$subName?></a></li>
 <?endforeach;?>
 		</ul>
 	</li>
 <?else:?>
-	<li><a href="<?=Kohana::config('admin.base').$path?>/"<?=strpos(url::current(), $path) !== FALSE ? ' class="selected"' : ''?>><?=$name?></a></li>
+	<li><a href="<?=join_paths(Kohana::config('admin.base'), $path)?>/"<?=strpos(url::current(), $path) !== FALSE ? ' class="selected"' : ''?>><?=$name?></a></li>
 <?endif;endforeach;?>
 		</ul>
 	</nav>
